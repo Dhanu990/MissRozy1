ba0e700e763239bd99f64f5afc67422b412d65af
Dhanu990 <130265533+Dhanu990@users.noreply.github.com>
2023-10-23 11:57:32 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -72,9 +72,10 @@ import sys
 
 MediaList = {}
 Bot.start()
-loop = asyncio.get_event_loop()
+loop = asyncio.new_event_loop()
+asyncio.set_event_loop(loop)
 
-PORT = "8080"
+PORT = "5000"
 
 async def Lazy_start():
     print('\n')
